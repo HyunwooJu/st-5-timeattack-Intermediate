eae0c34563ea5a0f5e0fa6384407dda170c8b375
HyunwooJu <soc05104@gmail.com>
2024-06-10 16:40:09 +0900

    calculator 미완료

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectTodosByCompletion } from "../../src/redux/slices/todoSlice";
 import TodoItem from "./TodoItem";
 
 export default function TodoList({ isDone }) {
+  const todos = useSelector((state) => selectTodosByCompletion(state, isDone));
+
   return (
     <section>
-      <h2>Working...</h2>
+      <h2>{isDone ? "Done..." : "Working..."}</h2>
       <ul>
-        <TodoItem />
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
       </ul>
     </section>
   );
--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,1 +1,31 @@
 // TODO: todoSlice 를 작성하세요.
+
+import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
+export default todoSlice.reducer;
+
+export const selectTodosByCompletion = createSelector(
+  [(state, isDone) => state.todos, (state, isDone) => isDone],
+  (todos, isDone) => todos.filter((todo) => todo.completed === isDone)
+);
